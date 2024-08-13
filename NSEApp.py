6a0f6b27c8c36d2cf911b492f27195408b206730
Neil Bagchi <65248311+NeilBag@users.noreply.github.com>
2024-08-14 01:38:19 +0530

    Update NSEApp.py

--- NSEApp.py
+++ NSEApp.py
@@ -152,5 +152,5 @@ def download_as_excel(n_clicks):
 
 # Run the app
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
+    port = int(os.environ.get('PORT', 8050))
     app.run_server(debug=True, host='0.0.0.0', port=port)
